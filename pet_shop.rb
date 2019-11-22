920d202d8e7320b1fbc0c61123ae96f6d50dd2d5
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2019-11-22 13:23:41 +0000

    2 completed

---
+++ pet_shop.rb
@@ -0,0 +1,11 @@
+def pet_shop_name (name)
+  return name[:name]
+end
+
+def total_cash(sum)
+  sum [:admin][:total_cash]
+end
+
+def add_or_remove_cash__add
+  @petshop [:admin][:total_cash] += 10
+end
