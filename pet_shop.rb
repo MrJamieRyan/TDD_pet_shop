20d99db60862db929ac8421127fd5d1d436df643
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2019-11-22 13:39:14 +0000

    Number 3 done

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,6 +6,7 @@ def total_cash(sum)
   sum [:admin][:total_cash]
 end
 
-def add_or_remove_cash__add
-  @petshop [:admin][:total_cash] += 10
+def add_or_remove_cash (pet_shop, money_added)
+pet_shop [:admin][:total_cash] += money_added
+pet_shop [:admin][:total_cash] += money_added
 end
