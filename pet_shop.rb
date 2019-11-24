107eb55bc1c7a018b10a9308bbd4d67a96da81a7
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2019-11-24 12:12:37 +0000

    Completed MVP

--- pet_shop.rb
+++ pet_shop.rb
@@ -30,8 +30,37 @@ def pets_by_breed(pet_shop, breed_type)
   return  breed_total
 end
 
-def find_pet_by_name (pet_shop, pet_name)
+def find_pet_by_name (pet_shop, pet_name_to_find)
   for each_pet in pet_shop[:pets]
-  return each_pet if each_pet[:name] == pet_name
+  return each_pet if each_pet[:name] == pet_name_to_find
 end
+return nil
+end
+
+def remove_pet_by_name (pet_shop, name_of_pet)
+  for each_pet in pet_shop[:pets]
+    if each_pet[:name] == name_of_pet
+      pet_shop[:pets].delete(each_pet)
+    end
+  end
+end
+
+def add_pet_to_stock(pet_shop, new_pet_name)
+  pet_shop[:pets].push(new_pet_name)
+end
+
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, amount_to_be_removed)
+  customer[:cash] -= amount_to_be_removed
+end
+
+def customer_pet_count (customer)
+  customer[:pets].length
+end
+
+def add_pet_to_customer(customer, new_pet)
+   customer[:pets].push(new_pet)
 end
